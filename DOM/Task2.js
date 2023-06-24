ae399f2f38ded6a17371679427fd57d1be13d8c8
Samoluk Volodumur Igorovish <steraria1@gmail.com>
2023-06-24 19:40:45 +0300

    HTML/CSS/JavaScript/Samoluk_Volodumur

---
+++ DOM/Task2.js
@@ -0,0 +1,8 @@
+var ulElement = document.getElementById("test"); 
+var liElement1 = document.createElement("li"); 
+var liElement2 = document.createElement("li"); 
+var liElement3 = document.createElement("li"); 
+
+ulElement.appendChild(liElement1);
+ulElement.appendChild(liElement2);
+ulElement.appendChild(liElement3);
